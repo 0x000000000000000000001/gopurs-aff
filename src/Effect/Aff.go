6221ddba9f141b337e712be5b59fdba576bfa074
0x1 <kevin.francart3@gmail.com>
2026-07-24 16:04:17 +0200

    chore: sync package.json purescript-npm updates

---
+++ src/Effect/Aff.go
@@ -0,0 +1,67 @@
+func _Pure(_ any) any {
+	panic("Not implemented: _pure")
+}
+
+func _ThrowError(_ any) any {
+	panic("Not implemented: _throwError")
+}
+
+func _CatchError(_ any, _ any) any {
+	panic("Not implemented: _catchError")
+}
+
+func _Fork(_ any, _ any) any {
+	panic("Not implemented: _fork")
+}
+
+func _Map(_ any, _ any) any {
+	panic("Not implemented: _map")
+}
+
+func _Bind(_ any, _ any) any {
+	panic("Not implemented: _bind")
+}
+
+func _Delay(_ any, _ any) any {
+	panic("Not implemented: _delay")
+}
+
+func _LiftEffect(_ any) any {
+	panic("Not implemented: _liftEffect")
+}
+
+func _ParAffMap(_ any, _ any) any {
+	panic("Not implemented: _parAffMap")
+}
+
+func _ParAffApply(_ any, _ any) any {
+	panic("Not implemented: _parAffApply")
+}
+
+func _ParAffAlt(_ any, _ any) any {
+	panic("Not implemented: _parAffAlt")
+}
+
+func _MakeFiber(_ any, _ any) any {
+	panic("Not implemented: _makeFiber")
+}
+
+func _MakeSupervisedFiber(_ any, _ any) any {
+	panic("Not implemented: _makeSupervisedFiber")
+}
+
+func _KillAll(_ any, _ any, _ any) any {
+	panic("Not implemented: _killAll")
+}
+
+func _Sequential(_ any) any {
+	panic("Not implemented: _sequential")
+}
+
+func GeneralBracket(_ any, _ any, _ any) any {
+	panic("Not implemented: generalBracket")
+}
+
+func MakeAff(_ any) any {
+	panic("Not implemented: makeAff")
+}
