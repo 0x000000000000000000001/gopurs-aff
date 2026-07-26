7b12b0b98cfe0c1f4405e122a6c43acfffcf0718
0x1 <kevin.francart3@gmail.com>
2026-07-26 16:08:22 +0200

    Auto-commit before architecture refactoring

--- src/Effect/Aff.go
+++ src/Effect/Aff.go
@@ -1,67 +1,67 @@
-func _Pure(_ any) any {
+func _Pure(_ interface{}) interface{} {
 	panic("Not implemented: _pure")
 }
 
-func _ThrowError(_ any) any {
+func _ThrowError(_ interface{}) interface{} {
 	panic("Not implemented: _throwError")
 }
 
-func _CatchError(_ any, _ any) any {
+func _CatchError(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _catchError")
 }
 
-func _Fork(_ any, _ any) any {
+func _Fork(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _fork")
 }
 
-func _Map(_ any, _ any) any {
+func _Map(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _map")
 }
 
-func _Bind(_ any, _ any) any {
+func _Bind(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _bind")
 }
 
-func _Delay(_ any, _ any) any {
+func _Delay(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _delay")
 }
 
-func _LiftEffect(_ any) any {
+func _LiftEffect(_ interface{}) interface{} {
 	panic("Not implemented: _liftEffect")
 }
 
-func _ParAffMap(_ any, _ any) any {
+func _ParAffMap(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _parAffMap")
 }
 
-func _ParAffApply(_ any, _ any) any {
+func _ParAffApply(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _parAffApply")
 }
 
-func _ParAffAlt(_ any, _ any) any {
+func _ParAffAlt(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _parAffAlt")
 }
 
-func _MakeFiber(_ any, _ any) any {
+func _MakeFiber(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _makeFiber")
 }
 
-func _MakeSupervisedFiber(_ any, _ any) any {
+func _MakeSupervisedFiber(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _makeSupervisedFiber")
 }
 
-func _KillAll(_ any, _ any, _ any) any {
+func _KillAll(_ interface{}, _ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _killAll")
 }
 
-func _Sequential(_ any) any {
+func _Sequential(_ interface{}) interface{} {
 	panic("Not implemented: _sequential")
 }
 
-func GeneralBracket(_ any, _ any, _ any) any {
+func GeneralBracket(_ interface{}, _ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: generalBracket")
 }
 
-func MakeAff(_ any) any {
+func MakeAff(_ interface{}) interface{} {
 	panic("Not implemented: makeAff")
 }
